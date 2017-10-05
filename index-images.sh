3c974efc86c962a2a1694565ec674539c9e5ffc9
Nelson Jeppesen <Nelson>
2017-10-05 13:21:31 -0700

    Handle yes / no on image

--- index-images.sh
+++ index-images.sh
@@ -10,23 +10,31 @@ IMAGES="$(find $INBOX -type f | grep -v PLACEHOLDER | egrep -iv '.raw$' | sed 's
 # Loop through all the non-raw images
 while read -r line; do
 
-  # Get the SHA of the non-raw image
-  filename=$(basename "$line")
-  extension="${filename##*.}"
-  filename="${filename%.*}"
-  SHA="$(sha1sum "$(echo $INBOX/$line)" | awk '{print $1}')"
+  python2.7 view-image.py "$INBOX/$line"
+  retcal=$?
+  if [ $retcal -gt 0 ] ; then
+    echo rm -v "$INBOX/$line"
+  else
+    # Get the SHA of the non-raw image
+    filename=$(basename "$line")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    SHA="$(sha1sum "$(echo $INBOX/$line)" | awk '{print $1}')"
 
-  cp -v "$INBOX/$line" processed/$SHA.$extension
-  RAW_IMAGES="$(find $inbox -name "$filename*" -type f -printf %f\\n | egrep -i '.raw$')"
+    cp -v "$INBOX/$line" processed/$SHA.$extension
+    RAW_IMAGES="$(find $inbox -name "$filename*" -type f -printf %f\\n | egrep -i '.raw$')"
 
-  while read -r line; do
-    if [ $line ] ; then 
-      filename=$(basename "$line")
-      extension="${filename##*.}"
-      filename="${filename%.*}"
-      cp -v "$INBOX/$line" processed/$SHA.$extension
-    fi
-  done <<< "$RAW_IMAGES"
+    while read -r line; do
+      if [ $line ] ; then 
+        filename=$(basename "$line")
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+        cp -v "$INBOX/$line" processed/$SHA.$extension
+      fi
+    done <<< "$RAW_IMAGES"
+  fi
+   echo $?
+
 
 done <<< "$IMAGES"
 
